8723a0ed95f728a7db108b2376d2a1fcf798109e
Dkingofcode <doladepo128@gmail.com>
2023-01-16 21:08:24 +0000

    added the 2-factor code

---
+++ script.py
@@ -0,0 +1,39 @@
+# go over to our gamil account and setup 2 factor authentication
+# generate app password
+# create a function to send the mail
+
+
+from email.message import EmailMessage
+
+email_sender = 'doladepo@gamil.com'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
