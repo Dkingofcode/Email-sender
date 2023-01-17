d96bb3b9f790797ad0ea0d1ae4db12168715f834
Dkingofcode <doladepo128@gmail.com>
2023-01-17 10:20:37 +0000

    created the logic for the email sending program

--- app2.py
+++ app2.py
@@ -1,1 +1,3 @@
 # ctqennjemjbfmwaa
+
+password = 'ctqennjemjbfmwaa'
--- script.py
+++ script.py
@@ -4,9 +4,31 @@
 
 
 from email.message import EmailMessage
+from app2 import password;
+import ssl
+import smtplib
 
-email_sender = 'doladepo@gamil.com'
+email_sender = 'doladepo128@gmail.com'
+email_password = password
 
+email_reciever = 'bevori4847@webonoid.com'
+
+subject = "Don't forget to subscribe"
+body = """
+When you watch a video, please hit subscribe
+"""
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_reciever
+em['subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_reciever, em.as_string())
 
 
 
